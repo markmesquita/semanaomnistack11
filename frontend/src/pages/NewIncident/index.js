8b86a611354cff5d7a09dd2a7af7c26bbc09acc3
Mark Mesquita <markmesquita@gmail.com>
2020-03-26 02:32:48 -0300

    feat: create React APP

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api'
+
+import logoImg from '../../assets/logo.svg'
+
+import './styles.css'
+
+export default function NewIncident() {
+  const [incident, setIncident] = useState({})
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [value, setValue] = useState('')
+  
+  const ongId = localStorage.getItem('ongId')
+
+  const history = useHistory()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const data = {
+      title,
+      description,
+      value,
+    }
+
+    try {
+      await api.post('incidents', 
+        data, 
+        { 
+          headers: {
+            Authorization: ongId
+          }
+        })
+
+      history.push('/profile')
+    } catch (error) {
+      alert('Ocorreu algum erro ao cadastrar o caso. Tente novamente')
+    }
+  }
+
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero"/>
+          <h1>Cadastrar novo caso</h1>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+          <Link className="back-link" to="/profile"><FiArrowLeft size={16} color="#E02041" />Voltar para a home</Link>
+        </section>
+        <form onSubmit={handleSubmit}>
+          <input 
+            placeholder="Titulo do caso"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <textarea 
+            placeholder="Descrição"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <input 
+            placeholder="Valor em reais " 
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+          <div>
+            <button type="reset" className="cancel">Cancelar</button>
+            <button type="submit" className="button">Cadastrar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { FiPower, FiTrash2 } from 'react-icons/fi'
+
+import api from '../../services/api'
+
+import logoImg from '../../assets/logo.svg'
+
+import './styles.css'
+
+export default function Profile() {
+  const [incidents, setIncidents] = useState([])
+  const [page, setPage] = useState(1)
+
+  const ongName = localStorage.getItem('ongName')
+  const ongId = localStorage.getItem('ongId')
+
+  const history = useHistory()
+
+  useEffect(() => {
+    if (!ongName && !ongId) {
+      history.push('/')
+    }
+  }, [])
+
+  useEffect(() => {
+    getProfile()
+  }, [])
+
+  const getProfile = () => {
+    api.get('profile', {
+      headers: {
+        Authorization: ongId
+      }
+    }).then(response => {
+      setIncidents(response.data)
+    })
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('ongName')
+    localStorage.removeItem('ongId')
+    history.push('/')
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      })
+
+      getProfile()
+    } catch (error) {
+      alert('Ocorreu algum erro ao deletar caso. Tente novamente.')
+    }
+  }
+
+  return (
+    <>
+    {(!ongName) ? (
+      <div></div>
+    ): (
+
+      <div className="profile-container">
+        <header>
+          <img src={logoImg} alt="Be The Hero"/>
+          <span>Bem vinda, {ongName}</span>
+
+          <Link className="button" to="/incidents/new">Cadastrar novo Caso</Link>
+          <button type="button" onClick={handleLogout}>
+            <FiPower size={18} color="#E02041" />
+          </button>
+        </header>
+
+        <h1>Casos Cadastrados</h1>
+
+        <ul>
+          {incidents.length !== 0 ? incidents.map(incident => (
+            <li key={incident.id}>
+            <strong>CASO:</strong>
+            <p>{incident.title}</p>
+            
+            <strong>DESCRIÇÃO:</strong>
+          <p>{incident.description}</p>
+
+            <strong>VALOR:</strong>
+            <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
+            
+            <button type="button" onClick={() => handleDelete(incident.id)}>
+              <FiTrash2 size={20} color="#A8A8B3" />
+            </button>
+          </li>
+          )) : (
+            <div>Nenhum caso cadastrado</div>
+          )}
+        </ul>
+      </div>
+    )}
+    </>
+  );
+}
